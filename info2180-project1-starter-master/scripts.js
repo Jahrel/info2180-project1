85d54fd572b11b32980f90524c493b1aabbb8879
Jahrel Mckenzie <jahr3lm@gmail.com>
2020-11-01 21:58:12 -0500

    Added Final changes of styles to weblayout

--- info2180-project1-starter-master/scripts.js
+++ info2180-project1-starter-master/scripts.js
@@ -1,1 +1,23 @@
 /* Add your JavaScript to this file */
+
+window.onload = function(){
+
+    var btn = document.getElementsByTagName("button")[0];
+    var message = document.getElementsByClassName("message")[0];
+    var email=document.getElementById("email");
+
+
+    btn.addEventListener("click", function(event) {
+
+        event.preventDefault();
+        if(email.value===""){
+
+            message.innerHTML = " Please enter a valid email address";
+        
+        }else{
+
+                message.innerHTML=" Thank You! Your email " + email.value + " has been added to our mailing list";
+        }
+
+    });
+}
